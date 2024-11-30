77661da12e2d14182c3139982fe40c9b2fcf2b24
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-11-30 22:41:56 +0600

    form with event handler

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,25 @@
 
 
-const SimpleForm = ()=>{
+const SimpleForm = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        console.log('form submitted');
+    }
+
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
                 <br />
-                <input type="button" value="submit" />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="password" name="password" />
+                <br />
+                <input type="submit" value="Submit" />
             </form>
         </div>
     );
