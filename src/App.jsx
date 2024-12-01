81f7b5541c95d5980571ef28e0026b206b6b2528
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-12-01 06:33:24 +0600

    re-useable form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 
 import './App.css'
-import RefForm from './components/RefForm/RefForm'
-import SimpleForm from './components/simpleForm/simpleForm'
-import StatefulForm from './components/StatefulForm/StatefulForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+//import HookForm from './components/HookForm/HookForm'
+//import RefForm from './components/RefForm/RefForm'
+//import SimpleForm from './components/simpleForm/simpleForm'
+//import StatefulForm from './components/StatefulForm/StatefulForm'
 
 
 function App() {
@@ -12,8 +14,25 @@ function App() {
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> 
       <StatefulForm></StatefulForm>
-      <RefForm></RefForm>*/}
-      
+      <RefForm></RefForm>
+      <HookForm></HookForm>*/}
+      <ReusableForm
+        formTitle={'Sign UP'}
+        handleSubmit={handleSingUpSubmit}>
+          <div>
+            <h2>Sing UP</h2>
+            <p>please sign up right now</p>
+          </div>
+        </ReusableForm>
+      <ReusableForm
+        formTitle={'Profile Update'}
+        handleSubmit={handleUpdateProfile}
+        submitBtnText='Update'>
+          <div>
+            <h2>Update Profile</h2>
+            <p>Always keep your profile updated</p>
+          </div>
+        </ReusableForm>
     </>
   )
 }
